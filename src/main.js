7943d4cf0622878396a3ef8af2ffcbef89c72bc4
liuning <liu_ning_belive@163.com>
2021-05-12 09:29:39 +0800

    测试 eslint ’

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router/index';
-import './style/index.scss';
+import './style/index.scss'
 
 const app = createApp(App)
-app.use(router)
-app.use(ElementPlus);
-app.mount('#app');
+app.use(router);
+app.use(ElementPlus)
+app.mount("#app");
