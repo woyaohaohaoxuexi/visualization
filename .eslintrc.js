34bd0201385f46a68b72561a8a3c2e1342f0d3b7
liuning <liu_ning_belive@163.com>
2021-05-12 09:21:28 +0800

    提eslint 文件修改提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,32 @@
+// import { resolve } from 'path'
+
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'airbnb-base',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'plugin:prettier/recommended'],
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: 'babel-eslint',
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: [
-    'vue',
+    'vue'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off', // 生产环境禁用consele
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off', // 生产环境禁用consele
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/no-export-in-script-setup': 0,
-    'prettier/prettier': 'error',
-    'import/no-unresolved': 'off'
+    'prettier/prettier': 'error'
+    // 'import/no-unresolved': 'off'
   },
+  settings: {
+    'import/resolver': {
+      alias: [
+        ['@', './src']
+      ]
+    }
+  }
 };
