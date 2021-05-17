7c8b8bd9accd1ab1281ebea3e09d5acd6cb3d9a6
liuning <pactera-440@crc.com.hk>
2021-05-17 21:07:09 +0800

    添加 .idea git忽略

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
-  <!--  <img alt="Vue logo" src="./assets/logo.png" />-->
   <routerView />
-  <!--  <HelloWorld msg="Hello Vue 3 + Vite" />-->
 </template>
 
 <script setup>
-// const a = 'test';
-// eslint-disable-next-line no-unused-vars
-// import HelloWorld from "./components/HelloWorld.vue";
 </script>
